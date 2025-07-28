3e4fe713da6485d441e202f804b8dcba8d0cc191
Jasmeet Kaur Laumbh <102907036+JasLaumbh@users.noreply.github.com>
2025-07-28 19:10:07 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,19 +1,19 @@
 #include <DHT.h>
 #include <Servo.h>
 
-#define DHTPIN   2
-#define SERVOPIN1 5
-#define SERVOPIN2 6
+#define DHTPIN     2
+#define DHTTYPE    DHT11
+#define SERVOPIN1  5
+#define SERVOPIN2  6
 
-DHT dht(DHTPIN, DHT11);
+DHT dht(DHTPIN, DHTTYPE);
 Servo servo1;
 Servo servo2;
+
 uint32_t ms, msD;
 float h = 0;
 float t = 0;
 float et = 0;
-
-int angle;
 
 void setup()
 {
@@ -30,50 +30,38 @@ void loop()
   {
     h = dht.readHumidity();
     t = dht.readTemperature();
-    et = t - 0.4*(t - 10)*(1.0 - (h/100));
 
-    Serial.print("Влажность: ");
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Error reading from DHT11");
+      return;
+    }
+
+    et = t - 0.4 * (t - 10) * (1.0 - (h / 100.0));
+
+    Serial.print("Humidity: ");
     Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Температура: ");
+    Serial.print(" %\tTemperature: ");
     Serial.print(t);
-    Serial.print(" *C\t");
-    Serial.print("Эффективная температура: ");
+    Serial.print(" *C\tEffective Temperature: ");
     Serial.print(et);
     Serial.println(" *C");
 
     msD = ms;
-  }
 
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("Ошибка считывания");
-    return;
-  }
-
-  if (et < 26)
-  {
-    servo1.write(95);
-    delay(300);
-    servo2.write(120);
-
-  }
-  if (et >= 26 && et <= 28)
-  {
-    servo2.write(70);
-    delay(100);
-
-    servo1.write(80);
-
-    
-  }
-  if (et > 28)
-  {
-    servo1.write(0);
-    delay(200);
-
-    servo2.write(118);
-
- 
+    if (et < 26) {
+      servo1.write(95);
+      delay(300);
+      servo2.write(120);
+    }
+    else if (et >= 26 && et <= 28) {
+      servo2.write(70);
+      delay(100);
+      servo1.write(80);
+    }
+    else if (et > 28) {
+      servo1.write(0);
+      delay(200);
+      servo2.write(118);
+    }
   }
 }
